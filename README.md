76025875f64da2ec6dc7098d56a31fbdfb14587b
Zeyu Deng <dengzeyu@gmail.com>
2021-11-08 17:24:21 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Kinetic Monte Carlo Simulation using Python (kmcPy)
+Source code to reproduce NASICON KMC simulation
+Author: Zeyu Deng
+Email: dengzeyu@gmail.com
+
+# Prerequisite Packages:
+python numpy scipy pandas numba tables pymatgen
+
+# Steps:
+1. Go into kmc folder
+2. python run_kmc.py comp structure_index T 
+comp is the parametric composition from 0 to 1, for Na1+xZr2SixP3-xO12, x = 3-3*comp
+structure_index can be any integer from 0 to 49
+T is temperature in Kelvin
+
+# Example
+python run_kmc.py 0.1 1 573
---
+++ event_kernal_generator/generate_events.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../lib')
+from kmc import KMC
+from kmc_tools import load_project,save_project
+from model import Event
+import numpy as np
+from numba.typed import List
+
+
+def generate_events(prim_fname,supercell_shape,lce_fname='../inputs/lce.pkl',lce_site_fname='../inputs/lce_site.pkl'):
+    import json
+    from pymatgen.core.structure import Structure
+    from pymatgen.analysis.local_env import CutOffDictNN
+    from pymatgen.core.lattice import Lattice
+    import multiprocessing
+    from joblib import Parallel, delayed
+    
+    print('Generating events ...')
+    shape = supercell_shape
+    
+    print('Initializing model with pirm.json at',prim_fname,'...')
+    with open(prim_fname,'r') as f:
+        prim = json.load(f)
+        prim_coords = [site['coordinate'] for site in prim['basis']]
+        prim_specie_cases = [site['occupant_dof'] for site in prim['basis']]
+        prim_lattice = Lattice(prim['lattice_vectors'])
+        prim_species = [s[0] for s in prim_specie_cases]
+        
+    supercell_shape_matrix = np.diag(supercell_shape)
+    print('Supercell Shape:\n',supercell_shape_matrix)
+    structure = Structure(prim_lattice,prim_species,prim_coords)
+    print('Converting supercell ...')
+    structure.remove_species(['Zr','O'])
+    structure.make_supercell(supercell_shape_matrix)
+    structure.to(fmt='cif',filename='supercell.cif')
+    local_cluster_expansion = load_project(lce_fname)
+    local_cluster_expansion_site = load_project(lce_site_fname)
+    # np.savetxt('frac_coords.txt',np.array(structure.frac_coords))
+    # np.savetxt('cart_coords.txt',np.array(structure.cart_coords))
+    # print(structure.lattice.matrix)
+    # np.savetxt('latt.txt',np.array(structure.lattice.matrix))
+    n_na1_per_prim = 2
+    n_na1 = n_na1_per_prim*np.prod(shape) 
+    local_env_cutoff_dict = {('Na','Na'):4,('Na','Si'):4}
+    ncpus = multiprocessing.cpu_count()
+    print('Looking for all possible swaps with ncpus =',ncpus,' (this might take a while) ...')
+    na_1_sites = structure[0:n_na1]
+    local_env_finder = CutOffDictNN(local_env_cutoff_dict)
+    local_env_info_list = list(Parallel(n_jobs=ncpus)(delayed(local_env_finder.get_nn_info)(structure,i) for i in np.arange(0,n_na1)))
+    events = []
+    for (na1_index,(na1_site,local_env_info)) in enumerate(zip(na_1_sites,local_env_info_list)):
+        for local_env in local_env_info:
+            if "Na" in local_env['site'].specie.symbol:
+                events.append(Event(na1_index,local_env['site_index'],local_env_info,local_cluster_expansion,local_cluster_expansion_site))
+    save_project(events,'./events.pkl')
+    
+    events_site_list = []
+    for event in events:
+        events_site_list.append(event.sorted_sublattice_indices)
+    
+    np.savetxt('./events_site_list.txt',np.array(events_site_list,dtype=int))
+    generate_event_kernal(len(structure))
+
+
+
+def generate_event_kernal(len_structure):
+    import subprocess, os
+    print('Computing site_event_list  ...')
+    home = os.getcwd()
+    subprocess.run(["rm","-f","generate_list.x"])
+    subprocess.run(["g++","-std=c++11","generate_list.cpp","-o","generate_list.x"])
+    subprocess.run(["./generate_list.x",str(len_structure)])
+    os.rename("results.csv",home+"/"+"event_kernal.csv")
+    os.chdir(home)
+
+supercell_shape = (2,1,1)
+lce_fname = '../inputs/lce.pkl'
+lce_site_fname = '../inputs/lce_site.pkl'
+
+prim_fname = '../inputs/prim.json'
+
+generate_events(prim_fname,supercell_shape,lce_fname,lce_site_fname)
