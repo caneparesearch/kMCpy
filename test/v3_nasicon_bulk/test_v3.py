e32d1b029f0396e075cc1ca4b66cb31f8e3b1111
DavidX <davidnormal@126.com>
2022-06-07 11:48:42 +0800

    add random number kernal

--- test/v3_nasicon_bulk/test_v3.py
+++ test/v3_nasicon_bulk/test_v3.py
@@ -104,6 +104,36 @@ class Test_version3(unittest.TestCase):
         
         
         # np.array((3.517242770690013e-06, 26.978226076495748, 3.187544456106211e-10, 1.2783794881088614e-10, 0.025760595723683707, 0.4010546380490277, 0.04309185078659044)) this is run from the given random number kernal and random number seed. This is a very strict criteria to see if the behavior of KMC is correct
+    def test_kmc_main_function_randomized(self):
+        from pathlib import Path
+        import os
+        current_dir= Path(__file__).absolute().parent
+        os.chdir(current_dir)
+        from kmcpy.io import InputSet,load_occ
+        from kmcpy.kmc import KMC
+        import numpy as np
+        api=2
+        inputset=InputSet.from_json("input/test_input_v3.json",api=2)
+
+        print(inputset._parameters.keys())
+        print(inputset._parameters["mc_results"])
+        inputset.parameter_checker()
+
+        inputset.set_parameter("occ",load_occ(fname=inputset._parameters["mc_results"],shape=inputset._parameters["supercell_shape"],select_sites=inputset._parameters["select_sites"],api=inputset.api,verbose=True))
+        inputset.set_parameter("use_numpy_random_kernel",False)
+        kmc = KMC(api=api)
+        events_initialized = kmc.initialization(**inputset._parameters) # v in 10^13 hz
+
+        # # step 2 compute the site kernal (used for kmc run)
+        kmc.load_site_event_list(inputset._parameters["event_kernel"])
+
+        # # step 3 run kmc
+        kmc_tracker=kmc.run_from_database(events=events_initialized,**inputset._parameters)
+        print(kmc_tracker.return_current_info())
+        self.assertFalse(np.allclose(np.array(kmc_tracker.return_current_info()),np.array((3.508959816621752e-06, 101.40523388197452, 1.8796478422471864e-09, 4.816490697215713e-10, 0.15190642462810805, 0.2562443128419963, 0.24387012300994337)),rtol=0.01,atol=0.01))
+        
+        
+        # np.array((3.517242770690013e-06, 26.978226076495748, 3.187544456106211e-10, 1.2783794881088614e-10, 0.025760595723683707, 0.4010546380490277, 0.04309185078659044)) this is run from the given random number kernal and random number seed. This is a very strict criteria to see if the behavior of KMC is correct
         
 if __name__ == '__main__':
     unittest.main()
