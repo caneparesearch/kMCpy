f8ad77aa274261142a01bed7a1e15b17087553c6
DavidX <davidnormal@126.com>
2022-06-08 14:19:21 +0800

    update wrapper

--- executable/wrapper.py
+++ executable/wrapper.py
@@ -21,7 +21,7 @@ import argparse
 
 
 
-def main(api=1,**kwargs):
+def main(api=3,**kwargs):
     """
     This is the wrapper for executing KMC
     
@@ -29,25 +29,21 @@ def main(api=1,**kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument('incar', metavar='N', type=str,help='path to the input.json')
     args = parser.parse_args()
-    inputset=InputSet.from_json(args.incar)
+    inputset=InputSet.from_json(args.incar,api=api)
     inputset.parameter_checker()
     # check if the parameter is good
-    if api==1:
-        """
-        what you need to do for 1st version API: calculate the occ, initialize, load, run_from_database
-        """
-        inputset.set_parameter("occ",load_occ(inputset._parameters["mc_results"],inputset._parameters["supercell_shape"],api=inputset.api))
-        # step 1 initialize global occupation and conditions
-        kmc = KMC()
-        events_initialized = kmc.initialization(**inputset._parameters) # v in 10^13 hz
 
-        # # step 2 compute the site kernal (used for kmc run)
-        kmc.load_site_event_list(inputset._parameters["event_kernel"])
+    inputset.set_parameter("occ",load_occ(inputset._parameters["mc_results"],inputset._parameters["supercell_shape"],api=inputset.api))
+    # step 1 initialize global occupation and conditions
+    kmc = KMC()
+    events_initialized = kmc.initialization(**inputset._parameters) # v in 10^13 hz
 
-        # # step 3 run kmc
-        kmc.run_from_database(events=events_initialized,**inputset._parameters)
-    if api>1:
-        raise NotImplementedError
+    # # step 2 compute the site kernal (used for kmc run)
+    kmc.load_site_event_list(inputset._parameters["event_kernel"])
+
+    # # step 3 run kmc
+    kmc.run_from_database(events=events_initialized,**inputset._parameters)
+
     pass
 
 
