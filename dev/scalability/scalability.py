faddf5b5bc63788fdf65b95f0f42ac537ee2993d
DavidX <davidnormal@126.com>
2022-06-28 20:56:12 +0800

    1

---
+++ dev/scalability/scalability.py
@@ -0,0 +1,70 @@
+import time
+class Test_version3():
+    def __init__(self,supercell=[2,2,2]) -> None:
+        self.supercell=supercell
+    
+    def time_test(self):
+        tick=time.time()
+        with open("./input/initial_state.json","w") as f:
+            occupation=[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]*self.supercell[0]*self.supercell[1]*self.supercell[2]
+            write_str="""{
+    "occupation" :  """+str(occupation)+""" 
+  }"""
+            f.write(write_str)
+
+        self.test_generate_events()
+        self.test_kmc_main_function()
+        tock=time.time()
+        print("elapsed time for ",self.supercell, tock-tick)    
+    
+    def test_generate_events(self):
+        from pathlib import Path
+        import os
+        current_dir= Path(__file__).absolute().parent
+        os.chdir(current_dir)
+        mobile_ion_identifier_type="label"
+        mobile_ion_specie_1_identifier="Na1"
+        mobile_ion_specie_2_identifier="Na2"
+        prim_cif_name="EntryWithCollCode15546_Na4Zr2Si3O12_573K.cif"
+        local_env_cutoff_dict={('Na+','Na+'):4,('Na+','Si4+'):4}
+        from kmcpy.event_generator import generate_events3
+
+        reference_local_env_dict=generate_events3(prim_cif_name=prim_cif_name,local_env_cutoff_dict=local_env_cutoff_dict,mobile_ion_identifier_type=mobile_ion_identifier_type,mobile_ion_specie_1_identifier=mobile_ion_specie_1_identifier,mobile_ion_specie_2_identifier=mobile_ion_specie_2_identifier,species_to_be_removed=["O2-","O","Zr4+","Zr"],distance_matrix_rtol=0.01,distance_matrix_atol=0.01,find_nearest_if_fail=False,convert_to_primitive_cell=True,export_local_env_structure=True,supercell_shape=self.supercell,event_fname="./input/events.json",event_kernal_fname='./input/event_kernal.csv',verbosity="INFO")
+        
+
+        
+  
+    def test_kmc_main_function(self):
+        from pathlib import Path
+        import os
+        current_dir= Path(__file__).absolute().parent
+        os.chdir(current_dir)
+        from kmcpy.io import InputSet,load_occ
+        from kmcpy.kmc import KMC
+        import numpy as np
+        api=3
+        inputset=InputSet.from_json("input/test_input_v3.json",api=api)
+
+        inputset.parameter_checker()
+
+        inputset.set_parameter("occ",load_occ(fname=inputset._parameters["mc_results"],shape=inputset._parameters["supercell_shape"],select_sites=inputset._parameters["select_sites"],api=inputset.api,verbose=True))
+        inputset.set_parameter("supercell_shape",self.supercell)
+        
+        kmc = KMC(api=api)
+        events_initialized = kmc.initialization(**inputset._parameters) # v in 10^13 hz
+
+        # # step 2 compute the site kernal (used for kmc run)
+        kmc.load_site_event_list(inputset._parameters["event_kernel"])
+
+        # # step 3 run kmc
+        kmc_tracker=kmc.run_from_database(events=events_initialized,**inputset._parameters)
+
+
+
+        
+
+        
+if __name__ == '__main__':
+    for i in range(1,12):
+        a=Test_version3(supercell=[i,i,i])
+        a.time_test()
