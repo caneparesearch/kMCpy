1e4d029d7f14ed044dc74ca8117d655fffc33169
DavidX <davidnormal@126.com>
2022-07-14 22:41:02 +0800

    kmc init

--- executable/gui_wrapper.py
+++ executable/gui_wrapper.py
@@ -62,27 +62,53 @@ def main():
 
 
     # kmc task
+
     kmc_parser = subs.add_parser(
         'kmc', help='generate files required for local cluster expansion')
-    kmc_parser.add_argument('CifPath',
-                             help='path to the cif file for generating local cluster expansion',
-                             type=str, widget='FileChooser',default="/Users/weihangxie/Documents/GitHub/kmcPy_dev/dev/v3_nasicon_bulk/EntryWithCollCode15546_Na4Zr2Si3O12_573K.cif")    
-
+    kmc_parser.add_argument("work_dir",default="/Users/weihangxie/Documents/GitHub/kmcPy_dev/dev/v3_nasicon_bulk")
+    kmc_parser.add_argument("v",default=5000000000000,type=int)
+    kmc_parser.add_argument("equ_pass",default=1,type=int)
+    kmc_parser.add_argument("kmc_pass",default=100,type=int)
+    kmc_parser.add_argument("supercell_shape",default="2,1,1")
+    kmc_parser.add_argument("fitting_results",default="./input/fitting_results.json",type=str)
+    kmc_parser.add_argument("fitting_results_site",default="./input/fitting_results_site.json",type=str)
+    kmc_parser.add_argument("lce_fname",default="./input/lce.json",type=str)
+    kmc_parser.add_argument("lce_site_fname",default="./input/lce_site.json",type=str)
+    kmc_parser.add_argument("prim_fname",default="./input/EntryWithCollCode15546_Na4Zr2Si3O12_573K.cif",type=str)
+    kmc_parser.add_argument("event_fname",default="./input/events.json",type=str)
+    kmc_parser.add_argument("event_kernel",default="./input/event_kernal.csv",type=str)
+    kmc_parser.add_argument("mc_results",default="./input/initial_state.json",type=str)
+    kmc_parser.add_argument("select_sites",default="0, 1, 2, 3, 4, 5, 6, 7, 12, 13, 14, 15, 16, 17",type=str)
+    kmc_parser.add_argument("T",default=298,type=int)
+    kmc_parser.add_argument("comp",default=1,type=int)
+    kmc_parser.add_argument("structure_idx",default=1,type=int)
+    kmc_parser.add_argument("api",default=3,type=int)
+    kmc_parser.add_argument("immutable_sites",default="Zr4+,O2-,O,Zr",type=str)
+    kmc_parser.add_argument("verbose",default=True,type=bool)
+    kmc_parser.add_argument("random_seed",default=12345,type=int)
+    kmc_parser.add_argument("--use_numpy_random_kernel",action="store_true")
+    kmc_parser.add_argument("convert_to_primitive_cell",choices=["yes","no"],default="yes")
+    kmc_parser.add_argument("q",default=1.0,type=float)
+    kmc_parser.add_argument("elem_hop_distance",default=3.47782,type=float)
+    kmc_parser.add_argument("dimension",default=3,type=int)
 
     args=parser.parse_args()
 
     #print(vars(args))
-    
+    if args.convert_to_primitive_cell=="yes":
+        args.convert_to_primitive_cell=True
+    else:
+        args.convert_to_primitive_cell=False 
+        
+
+           
     if args.command=="event":
         
         # species to be removed
         args.species_to_be_removed=args.species_to_be_removed.split(",")
         
         # convert to primitive cell
-        if args.convert_to_primitive_cell=="yes":
-            args.convert_to_primitive_cell=True
-        else:
-            args.convert_to_primitive_cell=False
+
             
         # cutoff cluster
         cutoff_cluster=[int(args.cutoff_for_pair_cluster),int(args.cutoff_for_triplet_cluster),int(args.cutoff_for_quadruplet_cluster)]
@@ -111,9 +137,20 @@ def main():
         a.to_json(args.events_output_dir+"/"+args.local_cluster_expansion_json)
         
         generate_events3(**vars(args))
+    if args.command=="kmc":
         
-        
-        
+        args.supercell_shape=[int(scale) for scale in args.supercell_shape.split(",")]  
+        args.select_sites=[int(scale) for scale in args.select_sites.split(",")]
+        args.immutable_sites=[scale for scale in args.immutable_sites.split(",")] 
+        os.chdir(args.work_dir)
+        print(vars(args))
+        occ=load_occ(fname=args.mc_results,shape=args.supercell_shape,select_sites=args.select_sites,api=args.api)
+        kmc=KMC()
+        events_initialized=kmc.initialization(occ=occ,**vars(args))
+        kmc.load_site_event_list(args.event_kernel)
+
+        # # step 3 run kmc
+        kmc.run_from_database(events=events_initialized,**vars(args))
 
 
 
