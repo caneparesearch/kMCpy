7173cec337c849306b8327e24054c708c371f1c4
Zeyu Deng (Jerry) <dengzeyu@gmail.com>
2023-04-18 18:22:11 +0800

    update

--- kmcpy/external/pymatgen_local_env.py
+++ kmcpy/external/pymatgen_local_env.py
@@ -4441,3 +4441,122 @@ def metal_edge_extender(mol_graph):
         for indices in sites.values():
             total_metal_edges += len(indices)
     return mol_graph
+
+class CutOffDictNNrange(NearNeighbors):
+    """
+    Jerry: Modified from CutOffDictNN in pymatgen so that it can search all pairs between a range of distances [d_min, d_max]
+
+    A basic NN class using a dictionary of fixed cut-off distances.
+    Only pairs of elements listed in the cut-off dictionary are considered
+    during construction of the neighbor lists.
+    Omit passing a dictionary for a Null/Empty NN class.
+    """
+
+    def __init__(self, cut_off_dict=None):
+        """
+        Args:
+            cut_off_dict (dict[str, float]): a dictionary
+            of cut-off distances, e.g. {('Fe','O'): [2.0,3.0]} for
+            a maximum Fe-O bond length between 2.0 and 3.0 Angstroms.
+            Bonds will only be created between pairs listed
+            in the cut-off dictionary.
+            If your structure is oxidation state decorated,
+            the cut-off distances will have to explicitly include
+            the oxidation state, e.g. {('Fe2+', 'O2-'): [2.0,3.0]}
+        """
+        self.cut_off_dict = cut_off_dict or {}
+        self.range_distances = range_distances
+        # for convenience  Jerry: added minimum and maximum
+        self._max_dist = 0.0
+        self._min_dist = 1e3
+        lookup_dict_max = defaultdict(dict)
+        for (sp1, sp2), dist in self.cut_off_dict.items():
+            lookup_dict_max[sp1][sp2] = dist
+            lookup_dict_max[sp2][sp1] = dist
+            if dist > self._max_dist:
+                self._max_dist = dist
+
+            lookup_dict_min[sp1][sp2] = dist
+            lookup_dict_min[sp2][sp1] = dist
+            if dist < self._min_dist:
+                self._min_dist = dist
+        self._lookup_dict_max = lookup_dict_max
+        self._lookup_dict_min = lookup_dict_min
+
+
+    @property
+    def structures_allowed(self):
+        """
+        Boolean property: can this NearNeighbors class be used with Structure
+        objects?
+        """
+        return True
+
+    @property
+    def molecules_allowed(self):
+        """
+        Boolean property: can this NearNeighbors class be used with Molecule
+        objects?
+        """
+        return True
+
+    @property
+    def extend_structure_molecules(self):
+        """
+        Boolean property: Do Molecules need to be converted to Structures to use
+        this NearNeighbors class? Note: this property is not defined for classes
+        for which molecules_allowed is False.
+        """
+        return True
+
+    @staticmethod
+    def from_preset(preset):
+        """
+        Initialise a CutOffDictNN according to a preset set of cut-offs.
+        Args:
+            preset (str): A preset name. The list of supported presets are:
+                - "vesta_2019": The distance cut-offs used by the VESTA
+                  visualisation program.
+        Returns:
+            A CutOffDictNN using the preset cut-off dictionary.
+        """
+        if preset == "vesta_2019":
+            cut_offs = loadfn(os.path.join(_directory, "vesta_cutoffs.yaml"))
+            return CutOffDictNNrange(cut_off_dict=cut_offs)
+
+        raise ValueError(f"Unrecognised preset: {preset}")
+
+    def get_nn_info(self, structure: Structure, n: int):
+        """
+        Get all near-neighbor sites as well as the associated image locations
+        and weights of the site with index n in structure.
+        Args:
+            structure (Structure): input structure.
+            n (int): index of site for which to determine near-neighbor
+                sites.
+        Returns:
+            siw (list of tuples (Site, array, float)): tuples, each one
+                of which represents a coordinated site, its image location,
+                and its weight.
+        """
+        site = structure[n]
+
+        neighs_dists = structure.get_neighbors(site, self._max_dist)
+
+        nn_info = []
+        for nn in neighs_dists:
+            n_site = nn
+            dist = nn.nn_distance
+            neigh_cut_off_dist_max = self._lookup_dict_max.get(site.species_string, {}).get(n_site.species_string, 0.0)
+            neigh_cut_off_dist_min = self._lookup_dict_min.get(site.species_string, {}).get(n_site.species_string, 0.0)
+            if dist < neigh_cut_off_dist_max and dist > neigh_cut_off_dist_min:
+                nn_info.append(
+                    {
+                        "site": n_site,
+                        "image": self._get_image(structure, n_site),
+                        "weight": dist,
+                        "site_index": self._get_original_site(structure, n_site),
+                    }
+                )
+
+        return nn_info
