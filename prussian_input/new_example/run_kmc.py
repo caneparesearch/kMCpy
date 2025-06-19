28777f90374d9eae4e34133f92240efc352bcdb4
Zeyu Deng <dengzeyu@gmail.com>
2025-06-20 01:16:52 +0800

    Refactor input handling in run_kmc.py; remove unused parameters and streamline KMC initialization

--- prussian_input/new_example/run_kmc.py
+++ prussian_input/new_example/run_kmc.py
@@ -15,19 +15,9 @@ logging.basicConfig(
 inputset = InputSet.from_json("kmc_input_files/kmc_input_test.json")
 
 print(inputset._parameters.keys())
-print(inputset._parameters["mc_results"])
-inputset.parameter_checker()
+print(inputset._parameters["initial_state"])
 
-inputset.load_occ()
+kmc = KMC.from_inputset(inputset)
 
-inputset.set_parameter("use_numpy_random_kernel", True)
-kmc = KMC()
-
-events_initialized = kmc.initialization(**inputset._parameters)  # v in 10^13 hz
-
-# # step 2 compute the site kernal (used for kmc run)
-kmc.load_site_event_list(inputset._parameters["event_kernel"])
-
-# # step 3 run kmc
-kmc_tracker = kmc.run_from_database(events=events_initialized, **inputset._parameters)
+kmc_tracker = kmc.run(inputset)
 # print(kmc_tracker.return_current_info())
