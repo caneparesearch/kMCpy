5b976d20d58bc7531f4a4e89f20c9fdc8f9463d0
Zeyu Deng <dengzeyu@gmail.com>
2025-06-20 17:04:09 +0800

    Refactor module docstrings to provide detailed descriptions of functionality and usage for Event, Fitting, InputSet, KMC, and LocalClusterExpansion classes.

--- kmcpy/event.py
+++ kmcpy/event.py
@@ -1,8 +1,5 @@
 """
-Event is a database storing site and cluster info for each migration event
-
-Author: Zeyu Deng
-Email: dengzeyu@gmail.com
+This module defines the Event class, which encapsulates the information and methods required to represent and analyze a migration event in a lattice-based simulation, such as those used in kinetic Monte Carlo (kMC) studies. The Event class manages indices of mobile ions, local environments, sublattice information, and provides methods for initializing, updating, and serializing event data. It also includes methods for calculating occupation, correlation, migration barriers, and probabilities associated with migration events.
 """
 
 import numpy as np
--- kmcpy/tracker.py
+++ kmcpy/tracker.py
@@ -1,11 +1,7 @@
 """
-Tracker is an object to track trajectories of diffusing ions
-
-Author: Zeyu Deng
-Email: dengzeyu@gmail.com
+This module defines a Tracker class for monitoring mobile ion species in kinetic Monte Carlo (kMC) simulations.
 """
 
-import numpy as np
 import numpy as np
 import pandas as pd
 from copy import copy
