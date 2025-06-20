91d3a6ccc7d7d7f8c53030e03bf692a8486b890e
Zeyu Deng <dengzeyu@gmail.com>
2025-06-21 02:21:20 +0800

    Update installation instructions and version retrieval method in documentation

--- kmcpy/_version.py
+++ kmcpy/_version.py
@@ -1,3 +1,8 @@
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # Python <3.8
+
 __all__ = ["__version__"]
 
-__version__ = "v0.1.4"
+__version__ = version("kmcpy")
